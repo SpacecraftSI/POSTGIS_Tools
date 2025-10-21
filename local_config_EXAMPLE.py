bd4fc9b179e34ba537153f286344b0ea6f597c29
Leh <lehsmallshaw@gmail.com>
2025-10-21 11:00:47 -0700

    Does the trick now. Need to move stuff out to helpers or otherwise where I will see repeats of functions across multiple tools.
    Not entirely happy with how the class is set up as it makes it a bit tricky to be working on multiple tools at the same time unless the class specifies 'per tool' basis...

--- local_config_EXAMPLE.py
+++ local_config_EXAMPLE.py
@@ -9,3 +9,10 @@ class AreaConfig:
     clip_file = "schema.area_table"
     grid_file = "schema.grid_table"
     overlap_file = "schema.overlap_to_remove"
+
+class IDConfig:
+    segment_id = 'segmentid'
+    grid_id = 'grid_id'
+
+class directories:
+    output_dir = f'path/to/output/directory'
--- segments_with_polygon_id.py
+++ segments_with_polygon_id.py
@@ -1,19 +1,14 @@
 # segments_with_polygon_id.py
-
 # 2025-10-20
-
 # Take segments from postgis database and attach a id based on a polygon -> works with polygon or grid.
-
 # This is targeted towards using with AIS Vessel Tracking data hence some of the built-in settings. Ideally I can make this a bit more modular down the road so it can work pretty flexibility.
-
 
 # import
 import os
 from datetime import datetime
 import psycopg2 as pg
 from osgeo import gdal
-from local_config import DBConfig, AreaConfig
-
+from local_config import DBConfig, AreaConfig, IDConfig, DirConfig
 
 # connects to the target database using local_config.py
 conn = pg.connect(host=DBConfig.HOST,
@@ -22,7 +17,6 @@ conn = pg.connect(host=DBConfig.HOST,
                   user=DBConfig.USER,
                   password=DBConfig.PASSWORD
                   )
-
 
 # Settings
 stat_filter = True
@@ -39,30 +33,34 @@ clip_area = AreaConfig.clip_file
 poly_area = AreaConfig.grid_file
 remove_overlap_polygon = AreaConfig.overlap_file
 
-
 # running configurations
-
-
+output_type = 'gpkg'
+output_dir = DirConfig.output_dir
 
 # list of year/months tables to export
-table_years = [2020]
+table_years = [2024]
 # table_months = [1,2,3,4,5,6,7,8,9,10,11,12]
-table_months = [1]
+table_months = [6,7,8]
 table_list = []
 
 def main():
-    print("\nStarting segments_with_polygon_id.py . . .\n")
+    print("\nStarting segments_with_polygon_id.py . . .\n\n")
     start_time = datetime.now()
     cursor = conn.cursor()
 
-    table_list = create_table_list(table_years,table_months)
+    table_list = create_table_list(table_years, table_months)
     print("Processing the following tables: ", table_list)
+
+    # used for file-naming later
+    polygon_name = poly_area.split(".")[-1]
 
     # per table loop
     for table in table_list:
+        loop_time = datetime.now()
         schema, tablename = table.split(".")
         print(f"\nStarting process on {tablename} from {schema} . . .")
         temp_table = f"temp_{tablename}"
+        intersect_table = f"intertemp_{tablename}"
 
         # confirms a spatial index exists for the source table
         sql = f"CREATE INDEX IF NOT EXISTS idx_{tablename} ON {table} USING gist (geom)"
@@ -70,18 +68,39 @@ def main():
         conn.commit()
 
         # filter by area or statistics or both
-        filter(cursor,table,temp_table)
+        apply_filter(cursor, table, temp_table)
 
         # remove overlaps with land polygon
         geo_overlap_filter(cursor, temp_table, remove_overlap_polygon)
 
         # intersect segments with polygon and assign each segment a polygon-id
+        polygon_intersect(cursor, temp_table, intersect_table, poly_area, IDConfig.segment_id, IDConfig.grid_id)
+
+        # export temporary table to file
+        filename = f"{polygon_name}_{tablename}"
+        exporter(output_dir, filename, intersect_table, output_type)
+
+        # clean up temporary tables
+        cleanup_sql = f"""
+            DROP TABLE IF EXISTS {temp_table};
+            DROP TABLE IF EXISTS {intersect_table};"""
+        cursor.execute(cleanup_sql)
+        conn.commit()
+        print(f"Cleaned up temporary tables for {tablename}.")
+
+        now = datetime.now()
+        duration = now - loop_time
+        hours, remainder = divmod(duration.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        print(f"\nFinished {tablename}, the runtime was: {hours}h {minutes}m {seconds}s")
 
     # end per table loop
 
     now = datetime.now()
     duration = now - start_time
-    print(f"\nTotal runtime was: {duration}")
+    hours, remainder = divmod(duration.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print(f"\n\nFinished!\n\nThe total runtime was: {hours}h {minutes}m {seconds}s\n")
 
 
 def create_table_list(years,months): # maybe move this to helpers as it'll probably get reused
@@ -96,13 +115,12 @@ def create_table_list(years,months): # maybe move this to helpers as it'll proba
     return table
 
 
-def filter(cursor, table, temp_table):
+def apply_filter(cursor, table, temp_table):
     # Base filters (attribute-based)
     base_filter = f"""
     sogkt < {sog}
     AND lenm < {length}
-    AND duration < {duration}
-    """
+    AND duration < {duration}"""
 
     # Start building the SQL as an appended list
     sql_parts = [f"DROP TABLE IF EXISTS {temp_table};",
@@ -118,8 +136,7 @@ def filter(cursor, table, temp_table):
 
     # Add WHERE clause if attribute filters are requested
     if stat_filter:
-        sql_parts.append(f"WHERE {base_filter} LIMIT 1000")
-        # TODO TAKE OUT LIMIT AFTER TESTING!
+        sql_parts.append(f"WHERE {base_filter}")
 
     # Combine into final SQL
     sql = "\n".join(sql_parts)
@@ -157,8 +174,64 @@ def geo_overlap_filter(cursor, temp_table, remove_overlap_polygon):
     conn.commit()
 
 
-def polygon_intersect(cursor, temp_table, poly_area):
-    pass
+def polygon_intersect(cursor, temp_table, intersect_table, poly_area, segmentid, gridid):
+    sql = f"""
+        DROP TABLE IF EXISTS {intersect_table};
+        CREATE TABLE {intersect_table} AS
+        SELECT 
+            b.{gridid},
+            (b.{gridid}::text || a.{segmentid}::text) AS combined_id,
+            a.*,
+            ST_Intersection(a.geom, b.geom) AS intersect_geom
+        FROM {temp_table} a
+        JOIN {poly_area} b
+          ON ST_Intersects(a.geom, b.geom);
+        """
+
+    cursor.execute(sql)
+    conn.commit()
+
+    drop_sql = f"ALTER TABLE {intersect_table} DROP COLUMN geom;"
+    cursor.execute(drop_sql)
+    rename_sql = f"ALTER TABLE {intersect_table} RENAME COLUMN intersect_geom TO geom;"
+    cursor.execute(rename_sql)
+    conn.commit()
+
+    recalc_sql = f"""UPDATE {intersect_table}
+                            SET duration = duration * (ST_LENGTH(geom) / lenm),
+                            lenm = ST_LENGTH(geom);"""
+
+    cursor.execute(recalc_sql)
+    conn.commit()
+
+    pk_sql = f"""ALTER TABLE {intersect_table} 
+                DROP CONSTRAINT IF EXISTS {intersect_table}_pkey;
+                ALTER TABLE {intersect_table} 
+                ADD PRIMARY KEY (combined_id);
+                """
+    cursor.execute(pk_sql)
+    conn.commit()
+
+
+def exporter(output_dir, filename, table, output_type):
+    # exports data that touches the aoi to file
+    output_file = (os.path.join(output_dir, (filename + "." + output_type)))
+
+    gdal.UseExceptions()
+    try:
+        ds = gdal.VectorTranslate(
+            destNameOrDestDS=output_file,
+            srcDS=f"PG:dbname={DBConfig.DATABASE} host={DBConfig.HOST} port={DBConfig.PORT} user={DBConfig.USER} password={DBConfig.PASSWORD}",
+            format=output_type,
+            layers=[table]
+        )
+    except Exception as e:
+        import traceback
+        print(f"Translation failed: {e}")
+        print(traceback.format_exc())
+
+    print(f"Exported product of {filename}, {table} as {output_type} in directory {output_dir}")
+
 
 if __name__ == "__main__":
     main()
